5adcb393ae3dc1b72f277ffc56507d579c64ae19
Sebastian Wilzbach <sebi@wilzbach.me>
2014-08-13 16:03:23 +0200

    added onAll

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
 var events = require("backbone-events-standalone");
+
+events.onAll = function(callback,context){
+  this.on("all", callback,context);
+  return this;
+};
+
+// Mixin utility
+events.mixin = function(proto) {
+  var exports = ['on', 'once', 'off', 'trigger', 'onAll', 'stopListening', 'listenTo',
+  'listenToOnce', 'bind', 'unbind'];
+  for(var i=0; i < exports.length;i++){
+    var name = exports[i];
+    proto[name] = this[name];
+  }
+  return proto;
+};
+
 module.exports = events;
